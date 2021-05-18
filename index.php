5257d2d6f3ecc0a6930c3d51e72972aa125db57f
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2021-05-18 09:10:14 -0700

    added views/home.html and index.php for default route to home.html, added h1 element to home.html

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// controller for the diner project
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once ('vendor/autoload.php');
+session_start();
+// instantiate Fat-Free
+$f3 = Base::instance();
+
+// define routes
+$f3->route('GET /', function (){
+    // instantiate a view object
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// run Fat-Free
+$f3->run();
