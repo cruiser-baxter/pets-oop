745bbe0e4f40a342de3c0e3649153bf660c64b1e
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2021-05-18 09:31:03 -0700

    added pet class

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// require vendor/autoload.php
 require_once ('vendor/autoload.php');
+// require pet class
+require ('pet.php');
 session_start();
+
+
 // instantiate Fat-Free
 $f3 = Base::instance();
 
@@ -14,8 +19,17 @@ $f3 = Base::instance();
 $f3->route('GET /', function (){
     // instantiate a view object
     $view = new Template();
-    echo $view->render('views/home.html');
+    // echo $view->render('views/home.html');
+    echo "<h1>Hello Pets!</h1>";
+
+    // instanciate a pet object
+    $pet = new Pet();
+
+    // invoke the eat method
+    $pet->eat();
+
 });
+
 
 // run Fat-Free
 $f3->run();
---
+++ pet.php
@@ -0,0 +1,20 @@
+<?php
+// controller for the diner project
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//
+class Pet
+{
+    private $_name;
+    private $_color;
+
+    // method eat()
+    function eat()
+    {
+        echo "Pet is eating!";
+    }
+
+}
